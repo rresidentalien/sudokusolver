3d2615e9a61dc56f2d67c67b4586fb9a2b40dd91
arc25275 <arc25275@gmail.com>
2024-09-08 11:25:07 -0700

    Add max limit to loop and add indicator to print

--- sudoku.c
+++ sudoku.c
@@ -6,21 +6,27 @@ int SIZE_ROWS = 9;
 int SIZE_COLUMNS = 9;
 
 int main() {
-    int **puzzle;
-    Cell ***cell;
+    int **puzzle, **puzzle_copy;
+    int i = 0;
+    Cell ***cell, ***cell_copy;
 
     puzzle = createPuzzle();
+    puzzle_copy = createPuzzle();
 
     cell = setUpPuzzle(puzzle);
+    cell_copy = setUpPuzzle(puzzle_copy);
 
-    printPuzzle(cell);
+    printPuzzle(cell, cell);
     printf("\n\n");
 
     do {
+        i++;
         checkPuzzle(cell);
-    }while (unsolved >= 1);
+    } while (unsolved >= 1 && i < 100);
 
-    printPuzzle(cell);
+
+    printPuzzle(cell, cell_copy);
+    printf("Iterations = %d\n", i);
 
     return 0;
 }
